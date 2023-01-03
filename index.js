e1ca01db645196238726d350f9cb91879cc121ec
Gabriela Sperb <gabrielasperb@gmail.com>
2023-01-03 14:13:07 -0300

    upload

---
+++ index.js
@@ -0,0 +1,12 @@
+document.getElementById("flexRadioDefault1").addEventListener("click", function () {
+    document.getElementById("validationCustom04").disabled = true;
+});
+document.getElementById("flexRadioDefault2").addEventListener("click", function () {
+    document.getElementById("validationCustom04").disabled = true;
+});
+document.getElementById("flexRadioDefault3").addEventListener("click", function () {
+    document.getElementById("validationCustom04").disabled = true;
+});
+
+
+
