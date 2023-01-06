de03144e9976db96490a4d2bf8c0db34729c0f04
Gabriela Sperb <108879411+gbspb@users.noreply.github.com>
2023-01-06 10:31:14 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
 document.getElementById("flexRadioDefault1").addEventListener("click", function () {
-    document.getElementById("validationCustom04").disabled = true;
+    document.getElementById("validationCustom").disabled = true;
 });
 document.getElementById("flexRadioDefault2").addEventListener("click", function () {
-    document.getElementById("validationCustom04").disabled = true;
+    document.getElementById("validationCustom").disabled = true;
 });
 document.getElementById("flexRadioDefault3").addEventListener("click", function () {
-    document.getElementById("validationCustom04").disabled = true;
+    document.getElementById("validationCustom").disabled = true;
 });
 
-
+document.getElementById("validationCustom").addEventListener("click", function () {
+    document.getElementById("flexRadioDefault3").disabled = true;
+});
+document.getElementById("validationCustom").addEventListener("click", function () {
+    document.getElementById("flexRadioDefault2").disabled = true;
+});
+document.getElementById("validationCustom").addEventListener("click", function () {
+    document.getElementById("flexRadioDefault1").disabled = true;
+});
 
